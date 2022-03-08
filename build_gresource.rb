21ea416f09acddfaf6dcd87389def8099f391132
mx-2 <mxm2@hotmail.de>
2022-03-08 17:14:53 +0100

    Add GDM wallpaper support.

--- build_gresource.rb
+++ build_gresource.rb
@@ -72,9 +72,12 @@ if File.file?("#{SRCDIR}/icons/no-notifications.svg")
 end
 
 css_filename = "#{WRKDIR}/theme/gnome-shell.css"
+css_file_prefix = css_filename.sub(/\.css$/, "")
 css = File.read("#{SRCDIR}/gnome-shell.css")
 css = css.gsub("overview-wallpaper.png", "file://#{File.expand_path("#{SRCDIR}/overview-wallpaper.png")}")
-File.write(css_filename, css)
+File.write("#{css_file_prefix}-dark.css", css)
+File.write("#{css_file_prefix}-high-contrast.css", css)
+File.write("#{css_file_prefix}-light.css", css)
 
 # Compile
 files = Dir.glob("#{WRKDIR}/theme/*").map do |file|
